3daa593e90efe2c4a2da453edbd3404528508fe8
Daksh <akshd@outlook.com>
2023-10-11 01:02:47 +0530

    chore: add dist files

---
+++ dist/index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+"use strict";
+const { Command } = require("commander");
+const figlet = require("figlet");
+const fs = require("fs");
+const path = require("path");
+const program = new Command();
+console.log(figlet.textSync("Solidity Interfacer"));
+program.version("0.0.1").description("A CLI Tool to get Solidity Interface from a contract").option("-p, --path  [value]", "File Path");
+program.parse(process.argv);
+const options = program.opts();
+const extractSolidityInterface = (filePath) => {
+    try {
+        const solidityCode = fs.readFileSync(filePath, 'utf-8');
+        const lines = solidityCode.split('\n');
+        let isInsideContract = false;
+        let interfaceContent = [];
+        let contractName = '';
+        for (const line of lines) {
+            if (!isInsideContract) {
+                if (line.trim().startsWith('contract ')) {
+                    isInsideContract = true;
+                    const contractParts = line.trim().split(' ');
+                    contractName = contractParts[1];
+                    interfaceContent.push(`pragma solidity ^0.8.19;`);
+                    interfaceContent.push(`interface I${contractName} {`);
+                }
+            }
+            else {
+                if (line.trim().startsWith('function ')) {
+                    const funcParts = line.trim().split(' ');
+                    if (funcParts.includes("{")) {
+                        funcParts.splice(funcParts.indexOf("{"), 1);
+                    }
+                    const funcDeclaration = funcParts.join(' ');
+                    interfaceContent.push(`    ${funcDeclaration}; `);
+                }
+            }
+        }
+        interfaceContent.push(`}`);
+        if (!contractName) {
+            throw new Error('No contract found in the Solidity file.');
+        }
+        return interfaceContent.join('\n');
+    }
+    catch (error) {
+        console.error(`Error reading or extracting contract interface from the Solidity file: ${error}`);
+        return null;
+    }
+};
+if (options.path) {
+    console.log("\n");
+    const filepath = `${__dirname}/${options.path}`;
+    console.log("File Loaded: ", filepath);
+    const iface = extractSolidityInterface(filepath);
+    console.log(iface);
+    // fs.writeFileSync(path.join(__dirname, `I${options.path}`), iface);
+    console.log("\n");
+    console.log("Successfully Wrote Interface to:", `${__dirname}/I${options.path}`);
+    console.log("\n");
+}
+//# sourceMappingURL=index.js.map
